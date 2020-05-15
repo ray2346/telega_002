6ebd2205ae61c046c5c31aeb5e8bf51babd8b68d
ray2346 <54332835+ray2346@users.noreply.github.com>
2020-05-15 10:37:41 +0300

    15/05.2020

--- jsindex.js
+++ jsindex.js
@@ -1,7 +1,7 @@
-const BotTelegram = require('node-telegram-bot-api')
-const TOKEN = '1084830404:AAEszzF1u3d8675TSh9qoFKJ_zWdRVADxdk'
-console.log('=======Красава БОТ Запущен======')
-const debag = require('./helpers')
+const BotTelegram = require('node-telegram-bot-api');
+const TOKEN = '1084830404:AAEszzF1u3d8675TSh9qoFKJ_zWdRVADxdk';
+console.log('=======Красава БОТ Запущен======');
+const debag = require('./helpers');
 const bot = new BotTelegram(TOKEN,{
     polling:{
         interval: 300,
@@ -11,16 +11,59 @@ const bot = new BotTelegram(TOKEN,{
         }}
 });
 
+const inline_keyboard=[
+    [{
+        text: 'Forward ',             //кнопка 1 1 строка
+        callback_data: 'forward'
+    }, {
+        text: 'Replay',              //кнопка 2 1 строка
+        callback_data: 'reply'
+    }],
+    [{
+        text: 'Edit',             //кнопка 3 2 строка
+        callback_data: 'edit'
+    }, {
+        text: 'Delete ',             //кнопка 4 2 строка
+        callback_data: 'delete'
+    }]
+]                          // Клавиатура в чате
 
+bot.on('callback_query', (query)=>{    //Обработчик событий
 
-bot.on('message',(msg) => {
-    const html =`<strong>
-<b> Hello, ${msg.from.first_name} </b>
-<i> Test message</i>
-<pre>${debag(msg)} </pre>
-</strong>`
-
-    bot.sendMessage(msg.chat.id,html,{
-        parse_mode: 'HTML'
+    const   {chat,message_id, text }= query.message
+    switch (query.data) {
+        case 'forward':
+            // куда,откуда,что
+            bot.forwardMessage(chat.id, chat.id, message_id)
+            break
+        case 'reply':
+            bot.sendMessage(chat.id,`Отвечаем на сообщение`,{
+                reply_to_message_id: message_id
+            })
+            break
+        case 'edit':
+            bot.editMessageText(`${text} (edited)`,{
+                chat_id:chat.id,
+                message_id: message_id,
+                reply_markup: {inline_keyboard}
+            })
+            break
+        case 'delete':
+            bot.deleteMessage(chat.id, message_id)
+            break
+    }
+    bot.answerCallbackQuery({
+        callback_query_id: query.id
     })
 })
+
+bot.onText(/\/start/,(msg,[source,match])=>{
+    const chatdId = msg.chat.id
+    bot.sendMessage(chatdId,'Keyboard',{
+        reply_markup: {
+            reply_keyboard:inline_keyboard
+        }
+    })
+})
+
+
